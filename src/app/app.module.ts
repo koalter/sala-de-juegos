f8c55595c18579cc492cc8fa6a7faa71220893a0
koalter <heavens_drive090@hotmail.com>
2022-09-05 00:14:27 -0300

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'sala-de-juegos-31e6d',
+    appId: '1:10086412169:web:ae44e01699411e3ef37b58',
+    storageBucket: 'sala-de-juegos-31e6d.appspot.com',
+    apiKey: 'AIzaSyDzzGrSf7qqjilCtAjx7GxExe7PtPqgT1c',
+    authDomain: 'sala-de-juegos-31e6d.firebaseapp.com',
+    messagingSenderId: '10086412169',
+    measurementId: 'G-MTJYP3Z925',
+  },
   production: true
 };
