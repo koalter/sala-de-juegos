ce915fee3bbf17ab11cb70c4c261012fbbf2173f
koalter <heavens_drive090@hotmail.com>
2022-09-27 04:10:35 -0300

    Ahorcado listo

--- src/app/components/home/ahorcado/ahorcado.component.ts
+++ src/app/components/home/ahorcado/ahorcado.component.ts
@@ -7,19 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AhorcadoComponent implements OnInit {
 
+  palabras : string[] = ['CIRCUNFERENCIA', 'ESQUELETO', 'QUIMICA', 'VERDURA', 'COMENSAL'];
   palabra : string[] = [];
   palabraOculta : string[] = [];
-  vida : number[] = [1, 2, 3, 4, 5, 6, 7];
+  vida : any[] = [1, 2, 3, 4, 5, 6, 7];
+  perdiste : boolean = false;
+  ganaste : boolean = false;
+  instrucciones : boolean = true;
+  keys : any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    const res = 'CIRCUNFERENCIA';
+    const res : string = this.palabras[Math.floor(Math.random() * this.palabras.length)];
     this.palabraOculta = res.split('');
     this.palabra = this.palabraOculta.map(v => v).fill('_');
+    this.ganaste = false;
+    this.perdiste = false;
+    this.vida = [1, 2, 3, 4, 5, 6, 7];
+
+    if (this.keys.length > 0) {
+      for (let key of this.keys) {
+        key.disabled = false;
+      }
+    }
   }
 
   seleccionarLetra(ev) {
+    this.keys.push(ev.target);
     ev.target.disabled = true;
     const char = ev.target.childNodes[0].textContent;
     let indices : number[] = [];
@@ -38,6 +53,14 @@ export class AhorcadoComponent implements OnInit {
 
     if (!existe) {
       this.vida.pop();
+    }
+    
+    if (this.vida.length <= 0) {
+      this.perdiste = true;
+    }
+
+    if (this.palabra.toString() === this.palabraOculta.toString()) {
+      this.ganaste = true;
     }
   }
 }
