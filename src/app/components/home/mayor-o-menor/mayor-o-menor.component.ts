4c422965aa741ef847958f9e1c605114f01e141f
koalter <heavens_drive090@hotmail.com>
2022-09-28 05:18:57 -0300

    La tarjeta anda!

--- src/app/components/home/mayor-o-menor/mayor-o-menor.component.ts
+++ src/app/components/home/mayor-o-menor/mayor-o-menor.component.ts
@@ -10,11 +10,38 @@ export class MayorOMenorComponent implements OnInit {
   instrucciones : boolean = true;
   ganaste : boolean = false;
   perdiste : boolean = false;
+  valorActual : number = 0;
+  valorSiguiente : number = 1;
+
+  mazo : number[] = this.llenarMazo();
 
   constructor() { }
 
   ngOnInit(): void {
-    this.instrucciones = false;
+    this.mazo = this.mezclar(this.mazo);
   }
 
+  private llenarMazo() : number[] {
+    return [
+      1,2,3,4,5,6,7,8,9,10,11,12,13,
+      1,2,3,4,5,6,7,8,9,10,11,12,13,
+      1,2,3,4,5,6,7,8,9,10,11,12,13,
+      1,2,3,4,5,6,7,8,9,10,11,12,13
+    ];
+  }
+
+  private mezclar(arr : number[]) {
+    let count = arr.length, temp, index;
+
+    while(count > 0) {
+        index = Math.floor(Math.random() * count);
+        count--;
+
+        temp = arr[count];
+        arr[count] = arr[index];
+        arr[index] = temp;
+    }
+
+    return arr;
+  }
 }
